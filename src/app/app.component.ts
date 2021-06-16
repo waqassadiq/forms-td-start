7890f11b434833e980da785ae143c49d0688022d
Waqas <wakassadik@gmail.com>
2021-06-16 21:14:17 +0500

    getting form whole object as ElementRef

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: ElementRef){
+    console.log(form);
+  }
+}
