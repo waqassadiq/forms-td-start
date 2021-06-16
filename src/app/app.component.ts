a662d1e896b10c1b15f6c86726415b49ecfdfe4d
Waqas <wakassadik@gmail.com>
2021-06-16 21:26:59 +0500

    alternative way to submit a form, using viewChilld

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms'
 @Component({
   selector: 'app-root',
@@ -6,11 +6,19 @@ import { NgForm } from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupFrom: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
+  /**
   onSubmit(form: NgForm){
     console.log(form);
+  }
+   */
+
+  onSubmit(){
+    console.log(this.signupFrom);
   }
 }
