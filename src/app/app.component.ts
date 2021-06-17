3ac5af348c3e89ff406dc1e04cf90ca310a841cc
Waqas <wakassadik@gmail.com>
2021-06-17 18:57:47 +0500

    using form data - to be displayed on page via object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer: String = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted =  false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -41,6 +50,12 @@ export class AppComponent {
    */
 
   onSubmit(){
+    this.submitted = true;
     console.log(this.signupFrom);
+    this.user.username = this.signupFrom.value.userData.username;
+    this.user.email = this.signupFrom.value.userData.email;
+    this.user.secretQuestion = this.signupFrom.value.secret;
+    this.user.answer = this.signupFrom.value.questionAnswer;
+    this.user.gender = this.signupFrom.value.gender;
   }
 }
